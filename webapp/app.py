22caad4ba972ccd54cfb537a35fd754a1fc34023
Olcay <olcayto2001@hotmail.com>
2026-07-06 22:01:21 +0200

    Remove unused Azure Function request

--- webapp/app.py
+++ webapp/app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-
 @app.route("/test-function")
 def test_function():
     return jsonify({
@@ -16,17 +15,6 @@ def test_function():
         "course": "Software Build Automation Tools",
         "server_time": "2026-07-06"
     })
-
-    try:
-        response = requests.get("https://sba-function-olcito-h5a3gudjcjefazc5.swedencentral-01.azurewebsites.net/api/GetServerTime")
-        return jsonify(response.json())
-
-    except Exception as e:
-        return jsonify({
-            "status": "Error",
-            "message": str(e)
-        }), 500
-
 
 if __name__ == "__main__":
     app.run(debug=True)
